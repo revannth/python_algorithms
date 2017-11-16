6872f5c50823a5276379dab814b9ea16002be05e
revannth.github.io <revannth1997@gmail.com>
2017-11-16 15:59:22 +0530

    Alternate power added

---
+++ classic_recursions/Alternate_power.py
@@ -0,0 +1,21 @@
+#Alternate_power.py
+
+
+#Uses the logic that x^n of a number is nothing but x*(x^(n//2))^2 for odd and (x^(n//2))^2 for even
+
+
+def altpow(x,n):
+
+	if n==0:
+		return 1
+
+	else :
+		partial = altpow(x,n//2)
+		result =  partial*partial
+		if n%2==0:
+			return result
+		else:
+			return x*result
+
+if __name__ == '__main__':
+	print altpow(2,3)
