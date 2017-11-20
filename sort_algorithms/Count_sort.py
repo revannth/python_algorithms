117d40382dd73c028c6c5801a2742b52b7bed2b7
revannth.github.io <revannth1997@gmail.com>
2017-11-20 23:59:46 +0530

    Count sort added

---
+++ sort_algorithms/Count_sort.py
@@ -0,0 +1,24 @@
+#Count_sort.py
+
+#Works on the principle of sorting data given a large array of repeating numbers
+
+def csort(arr):
+	counter = [0]*length(arr)
+	for k in arr:
+		counter[k]+=1
+
+	ndx = 0
+	for i in range(0,length(arr)):
+		while(0<counter[i]):
+			a[ndx] = i
+			counter[i]-=1
+			ndx-=1
+
+
+if __name__=='__main__':
+	ar = [2,2,2,2,6,7,8,4,4,4,3,2,9,5,9,6,6]
+	csort(ar)
+
+
+
+
