1719b9d8771d0a73116f9e5745e584f120a8913f
revannth.github.io <revannth1997@gmail.com>
2017-11-21 00:04:14 +0530

    Count sort corrected

--- sort_algorithms/Count_sort.py
+++ sort_algorithms/Count_sort.py
@@ -3,21 +3,23 @@
 #Works on the principle of sorting data given a large array of repeating numbers
 
 def csort(arr):
-	counter = [0]*length(arr)
+	counter = [0]*len(arr)
 	for k in arr:
 		counter[k]+=1
 
 	ndx = 0
-	for i in range(0,length(arr)):
+	for i in range(0,len(arr)):
 		while(0<counter[i]):
-			a[ndx] = i
+			arr[ndx] = i
 			counter[i]-=1
-			ndx-=1
+			ndx+=1
+			
 
 
 if __name__=='__main__':
 	ar = [2,2,2,2,6,7,8,4,4,4,3,2,9,5,9,6,6]
 	csort(ar)
+	print ar
 
 
 
