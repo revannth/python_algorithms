580cd8dd58c985bcfdcd3552ba8fd210625ba357
revannth.github.io <revannth1997@gmail.com>
2017-11-24 08:43:05 +0530

    Disk_Utilization added

---
+++ classic_recursions/Disk_utilization.py
@@ -0,0 +1,15 @@
+#Disk_utilization.py
+
+#This is assuming each directory level to be a number
+
+def Diskutil(pa_th):
+	total=pa_th
+	if pa_th>1:
+		for i in range(1,pa_th):
+			total +=Diskutil(pa_th-1)
+	return total
+
+
+if __name__ == '__main__':
+	
+	print Diskutil(10)
