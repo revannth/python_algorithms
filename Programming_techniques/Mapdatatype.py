24eae0146b2d4ded47d0e37b3202233abdd14cef
revannth.github.io <revannth1997@gmail.com>
2018-02-18 23:50:01 +0530

    Map datatype added

--- Programming_techniques/Mapdatatype.py
+++ Programming_techniques/Mapdatatype.py
@@ -6,9 +6,18 @@
 
 freq = {}
 
-with open('/Users/vedalasrinivas/Documents/communicando_website/app/templates/Our_team1.html') as f:
-	main_string = f.read().lower
+for piece in open('/Users/vedalasrinivas/Documents/communicando_website/app/templates/Our_team1.html').read().lower().split():
+	word = ''.join(c for c in piece if c.isalpha())
+	if word:
+		freq[word] = 1 + freq.get(word,0)
+max_count = 0
+max_word = ''
 
+for (w,c) in freq.items():
+	if c>max_count:
+		max_count=c
+		max_word=w
 
-print(str(main_string))
+print(max_word,max_count)
+
 
