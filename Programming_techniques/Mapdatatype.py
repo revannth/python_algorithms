f005f5ebba710b5c07752479f954c33d1775da74
revannth.github.io <revannth1997@gmail.com>
2018-02-18 23:42:49 +0530

    Linked list and queues added

---
+++ Programming_techniques/Mapdatatype.py
@@ -0,0 +1,14 @@
+#Mapdatatype.py
+
+
+#Map for word count
+
+
+freq = {}
+
+with open('/Users/vedalasrinivas/Documents/communicando_website/app/templates/Our_team1.html') as f:
+	main_string = f.read().lower
+
+
+print(str(main_string))
+
