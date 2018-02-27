129df5c4b0da4199d86b2fe3fc47292dde434ef0
revannth.github.io <revannth1997@gmail.com>
2018-02-28 02:43:28 +0530

    Stack program added

---
+++ Programming_techniques/Stack.py
@@ -0,0 +1,41 @@
+#Stack.py
+
+class Stack:
+
+	def __init__(self):
+		self.stack = []
+
+	def push(self,number):
+
+		self.stack.append(number)
+
+	def pop(self):
+
+		return self.stack.pop()
+
+	def top(self):
+
+		return self.stack[-1]
+
+	def is_empty(self):
+
+		if len(self.stack)==0:
+			return True
+		return False
+	def __len__(self):
+
+		return len(self.stack)
+
+
+
+if __name__ == '__main__':
+	stck = Stack()
+	stck.push(20)
+	stck.push(40)
+	stck.push(100)
+	print(len(stck))
+	stck.pop()
+	print(len(stck))
+	stck.push(120)
+	print(stck.top())
+
